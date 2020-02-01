4065fdea8632fafa32e0852e58d9551d8ef9ee46
ArmenAtalyan <atalyan3@gmail.com>
2020-02-01 10:36:39 +0400

    write counter, which count how many times studentes first name and last name repeat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
-public class Main {
-    public static void main(String[] args) {
+import model.Student;
+import service.StudentService;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+public class Main {
+    public static void main(String[] args){
+        List<Student> list = new ArrayList<>();
+        Student student1 = new Student("Vardan", "Aramyan", 22,
+                "098225566", "Math");
+        Student student2 = new Student("Vardan", "Aramyan", 20,
+                "098225565", "Economic");
+        Student student3 = new Student("Satik", "Vardanyan", 29,
+                "098325566", "History");
+        Student student4 = new Student("Satik", "Vardanyan", 31,
+                "098224566", "Sociology");
+        Student student5 = new Student("Aram", "Khazaryan", 18,
+                "091225566", "Journalistic");
+        Student student6 = new Student("Ani", "Sahakyan", 23,
+                "041225566", "Philology");
+
+        list.add(student1);
+        list.add(student2);
+        list.add(student3);
+        list.add(student4);
+        list.add(student5);
+        list.add(student6);
+
+        Map<String, Integer> map = StudentService.countSameName(list);
+        for(Map.Entry<String, Integer> entry: map.entrySet()){
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println(key + " " + value);
+        }
     }
 }
--- src/service/StudentService.java
+++ src/service/StudentService.java
@@ -1,3 +1,23 @@
 package service;
+import model.Student;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class StudentService {
+
+    public static Map<String, Integer> countSameName(List<Student> list){
+        Map<String, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < list.size(); i++){
+            String temp = list.get(i).getFirstName()+" "+list.get(i).getLastName();
+            if(map.containsKey(temp)){
+                Integer count = map.get(temp);
+                map.replace(temp, count+1);
+            }
+            else
+                map.put(temp, 1);
+        }
+        return map;
+    }
+}
